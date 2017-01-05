58aafec5890180acf6745d6b3907d5d3491e078f
mperroy <mathieu.perroy@gmail.com>
2017-01-05 15:15:27 +0100

    Ajout de commentaire (à finir)

--- util/maputil.c
+++ util/maputil.c
@@ -15,59 +15,79 @@ void verification(int condition, char * message) {
 
 int main (int argc, char** argv)
 {
+	//on vérifie la bonne utilisation de la commande
 	if (argc < 3){
 		fprintf(stderr,"Usage : %s <file> <option>",argv[0]);
 		exit(2);
 	}
-	ssize_t r;
-	if(strcmp(argv[2], "--getwidth") == 0){
+	ssize_t r;//variable de vérification de la valeur de retour de read()
+	//utilisation de l'option --getwidth
+	if(strcmp(argv[2], "--getwidth") == 0) {
+		//on ouvre le fichier passé en paramètre
 		int fd = open(argv[1],O_RDONLY);
 		int width;
+		//on lit la valeur de la largeur de la carte
 		if ((r = read(fd,&width,sizeof(int))) == 0) return 1;
+		//on l'affiche
 		printf("Largeur : %d\n", width);
 		close(fd);
 		return 0;
 	}
-	else if (strcmp(argv[2], "--getheight") == 0){
+	//utilisation de l'option --getheight
+	else if (strcmp(argv[2], "--getheight") == 0) {
+		//on ouvre le fichier passé en paramètre
 		int fd = open(argv[1],O_RDONLY);
 		int height;
-		lseek(fd,sizeof(int),SEEK_SET); /* On se deplace d'un entier dont on ne connait pas la valeur*/
+		lseek(fd,sizeof(int),SEEK_SET); //On se deplace d'un entier dont on ne connait pas la valeur
+		//on lit la valeur de la hauteur de la carte
 		if ((r = read(fd,&height,sizeof(int))) == 0) return 1;
+		//on l'affiche
 		printf("Hauteur : %d\n",height);
 		close(fd);
 		return 0;
 	}
-	else if (strcmp(argv[2], "--getobjects") == 0){
+	//utilisation de l'option --getobjects
+	else if (strcmp(argv[2], "--getobjects") == 0) {
+		//on ouvre le fichier passé en paramètre
 		int fd = open(argv[1],O_RDONLY);
 		int nb_objects;
-		lseek(fd,2*sizeof(int),SEEK_SET); //On se déplace de deux entiers pour extraire le nombre d'objets.
+		lseek(fd,2*sizeof(int),SEEK_SET); //On se déplace de deux entiers pour extraire le nombre d'objets
+		//on lit le nombre d'objet sur la carte
 		if ((r = read(fd,&nb_objects,sizeof(int))) == 0) return 1;
+		//on l'affiche
 		printf("Nombre d'objets : %d\n",nb_objects);
 		close(fd);
 		return 0;
 	}
-	else if (strcmp(argv[2], "--getinfo") == 0){
+	//utilisation de l'option --getinfo
+	else if (strcmp(argv[2], "--getinfo") == 0) {
+		//on ouvre le fichier passé en paramètre
 		int fd = open(argv[1],O_RDONLY);
 		int width,height, nb_objetcs;
+		//on lit la largeur, la hauteur et le nombre d'objet de la carte
 		if ((r = read(fd,&width,sizeof(int))) == 0) return 1;
 		if ((r = read(fd,&height,sizeof(int))) == 0) return 1;
 		if ((r = read(fd,&nb_objetcs,sizeof(int))) == 0) return 1;
+		//on affiche les informations récupérées
 		printf("Informations sur %s : \nHauteur : %d\nLargeur : %d\nNombre d'objets : %d\n", argv[1],height,width, nb_objetcs);
 		close(fd);
 		return 0;
 	}
-	else if (strcmp(argv[2], "--setwidth") == 0){
+	//utilisation de l'option --setwidth
+	else if (strcmp(argv[2], "--setwidth") == 0) {
+		//on vérifie la bonne utilisation de la commande
 		if (argc != 4){
 			fprintf(stderr, "Please enter a single number for width."); return 1;
 		}
 		int new_width = atoi(argv[3]);
-		if(new_width < MIN_WIDTH || new_width > MAX_WIDTH){
+		//on verifie que la nouvelle valeur de la largeur est possible
+		if(new_width < MIN_WIDTH || new_width > MAX_WIDTH) {
 			fprintf(stderr, "Minimum width : 16\n"
 					"Maximum width : 1024\n"
 					"Please enter a correct width.");
 			return 1;
 		}
-
+		//on ouvre le fichier passé en paramètre
 		int fd = open(argv[1],O_RDONLY);
 		verification(fd != -1, "[--setwidth] open");
 
@@ -76,13 +96,13 @@ int main (int argc, char** argv)
 		int nb_objects;
 
 		int verif_read;
-
+		//on lit l'ancienne valeur de la largeur
 		verif_read = read(fd, &old_width, sizeof(int));
 		verification(verif_read != -1, "[--setwidth] read old width");
-
+		//on lit la valeur de la hauteur
 		verif_read = read(fd, &height, sizeof(int));
 		verification(verif_read != -1, "[--setwidth] read height");
-
+		//on lit le nombre d'objets
 		verif_read = read(fd, &nb_objects, sizeof(int));
 		verification(verif_read != -1, "[--setwidth] read nb objects");
 
@@ -90,13 +110,13 @@ int main (int argc, char** argv)
 		int new_matrix[new_width][height];
 		int old_matrix[old_width][height];
 		int type;
-
+		//on initialise la nouvelle matrice à MAP_OBJECT_NONE (= -1)
 		for (int y = 0; y < height; ++y) {
 			for (int x = 0; x < new_width; ++x) {
 				new_matrix[x][y] = -1;
 			}
 		}
-
+		//on récupère toutes les valeurs de l'ancienne matrice
 		for (int y = 0; y < height; ++y) {
 			for (int x = 0; x < old_width; ++x) {
 				verif_read = read(fd, &type, sizeof(int));
@@ -104,7 +124,8 @@ int main (int argc, char** argv)
 				old_matrix[x][y] = type;
 			}
 		}
-
+		//si la nouvelle largeur est plus grande ou égale à l'ancienne
+		//on recopie toutes les valeurs de l'ancienne matrice dans la nouvelle matrice
 		if (old_width <= new_width) {
 			for (int y = 0; y < height; ++y) {
 				for (int x = 0; x < old_width - 1; ++x) {
@@ -115,7 +136,7 @@ int main (int argc, char** argv)
 			for (int x = old_width - 1; x < new_width; ++x) {
 				new_matrix[x][height - 1] = 0;
 			}
-		} else {
+		} else { //si on a diminué la largeur, on récupère toutes les valeurs avec la nouvelle largeur
 			for (int y = 0; y < height; ++y) {
 				for (int x = 0; x < new_width; ++x) {
 					new_matrix[x][y] = old_matrix[x][y];
@@ -130,19 +151,21 @@ int main (int argc, char** argv)
 		char path[nb_objects][256];
 		unsigned frames[nb_objects];
 		int properties[nb_objects][4];
-
+		//pour chaque objet
 		for (int i = 0; i < nb_objects; ++i) {
+			//on lit la longueur du chemin 
 			verif_read = read(fd, &path_length[i], sizeof(int));
 			verification(verif_read != -1, "[--setwidth] read path length");
 
 			for (int j = 0; j <= path_length[i]; ++j) {
+				//on lit le chemin 
 				verif_read = read(fd, &path[i][j], sizeof(char));
 				verification(verif_read != -1, "[--setwidth] read path");
 			}
-
+			//on lit son nombre de frames
 			verif_read = read(fd, &frames[i], sizeof(unsigned));
 			verification(verif_read != -1, "[--setwidth] read nb frames");
-
+			//on lit toutes ses propriétés
 			for (int k = 0; k < 4; ++k) {
 				verif_read = read(fd, &properties[i][k], sizeof(int));
 				verification(verif_read != -1, "[--setwidth] read properties");
@@ -150,57 +173,64 @@ int main (int argc, char** argv)
 		}
 
 		close(fd);
+		//on ouvre le ficher en paramètre en écriture
 		fd = open(argv[1], O_WRONLY|O_TRUNC, 0600);
 		int verif_write;
-
+		//on écrit la nouvelle largeur 
 		verif_write = write(fd, &new_width, sizeof(int));
 		verification(verif_write != -1, "[--setwidth] write new width");
-
+		//on écrit la hauteur 
 		verif_write = write(fd, &height, sizeof(int));
 		verification(verif_write != -1, "[--setwidth] write height");
-
+		//on écrit le nombre d'objets présents 
 		verif_write = write(fd, &nb_objects, sizeof(int));
 		verification(verif_write != -1, "[--setwidth] write nb objects");
-
+		//on écrit pour chaque coordonées (x,y) de la carte son type
 		for (int y = 0; y < height; ++y) {
 			for (int x = 0; x < new_width; ++x) {
 				verif_write = write(fd, &new_matrix[x][y], sizeof(int));
 				verification(verif_write != -1, "[--setwidth] write new matrix");
 			}
 		}
-
+		//pour chaque objet
 		for (int i = 0; i < nb_objects; ++i) {
+			//on écrit la longueur de son chemin
 			verif_write = write(fd, &path_length[i], sizeof(int));
 			verification(verif_write != -1, "[--setwidth] write path length");
-
+			//on écrit son chemin
 			for (int j = 0; j <= path_length[i]; ++j) {
 				verif_write = write(fd, &path[i][j], sizeof(char));
 				verification(verif_write != -1, "[--setwidth] write path");
 			}
-
+			//on écrit son nombre de frames
 			verif_write = write(fd, &frames[i], sizeof(unsigned));
 			verification(verif_write != -1, "[--setwidth] write frames");
 
 			for (int k = 0; k < 4; ++k) {
+				//on écrit ses propriétés
 				verif_write = write(fd, &properties[i][k], sizeof(int));
 				verification(verif_write != -1, "[--setwidth] write properties");
 			}
 		}
+		//on ferme le fichier
 		close(fd);
 		return 0;
 	}
-	else if (strcmp(argv[2], "--setheight") == 0){
-		if (argc != 4){
+	//utilisation de l'option --setheight
+	else if (strcmp(argv[2], "--setheight") == 0) {
+		//on vérifie la bonne utilisation de la commande
+		if (argc != 4) {
 			fprintf(stderr, "Please enter a single number for height."); return 1;
 		}
+		//on verifie que la nouvelle valeur de la largeur est possible
 		int new_height = atoi(argv[3]);
-		if(new_height < MIN_HEIGHT || new_height > MAX_HEIGHT){
+		if(new_height < MIN_HEIGHT || new_height > MAX_HEIGHT) {
 			fprintf(stderr, "Minimum height : 12\n"
 					"Maximum height : 20\n"
 					"Please enter a correct height.");
 			return 1;
 		}
-
+		//on ouvre le fichier en paraamètre en lecture
 		int fd = open(argv[1],O_RDONLY);
 		verification(fd != -1, "[--setheight] open");
 
@@ -209,13 +239,13 @@ int main (int argc, char** argv)
 		int nb_objects;
 
 		int verif_read;
-
+		//on lit la largeur
 		verif_read = read(fd, &width, sizeof(int));
 		verification(verif_read != -1, "[--setheight] read old width");
-
+		//on lit l'ancienne valeur de la hauteur
 		verif_read = read(fd, &old_height, sizeof(int));
 		verification(verif_read != -1, "[--setheight] read height");
-
+		//on lit le nombre d'objet
 		verif_read = read(fd, &nb_objects, sizeof(int));
 		verification(verif_read != -1, "[--setheight] read nb objects");
 
@@ -223,13 +253,13 @@ int main (int argc, char** argv)
 		int new_matrix[width][new_height];
 		int old_matrix[width][old_height];
 		int type;
-
+		//on initialise les valeurs de la nouvelle matrice
 		for (int y = 0; y < new_height; ++y) {
 			for (int x = 0; x < width; ++x) {
 				new_matrix[x][y] = -1;
 			}
 		}
-
+		//on récupère les valeurs de l'ancienne matrice
 		for (int y = 0; y < old_height; ++y) {
 			for (int x = 0; x < width; ++x) {
 				verif_read = read(fd, &type, sizeof(int));
@@ -237,7 +267,8 @@ int main (int argc, char** argv)
 				old_matrix[x][y] = type;
 			}
 		}
-
+		//si la nouvelle hauteur est plus grande ou égale à l'ancienne
+		//on recopie toutes les valeurs de l'ancienne matrice dans la nouvelle matrice
 		if (old_height <= new_height) {
             int offset_height = new_height - old_height;
 			for (int y = 0; y < old_height; ++y) {
@@ -249,7 +280,7 @@ int main (int argc, char** argv)
 				new_matrix[0][y] = 1;
 				new_matrix[width - 1][y] = 1;
 			}
-		} else {
+		} else {//si on a diminué la hauteur, on récupère toutes les valeurs avec la nouvelle hauteur
             int offset_height = old_height - new_height;
 			for (int y = 0; y < new_height; ++y) {
 				for (int x = 0; x < width; ++x) {
@@ -264,69 +295,76 @@ int main (int argc, char** argv)
 		char path[nb_objects][256];
 		unsigned frames[nb_objects];
 		int properties[nb_objects][4];
-
+		//pour chaque les objets
 		for (int i = 0; i < nb_objects; ++i) {
+			//on lit la longueur de son chemin
 			verif_read = read(fd, &path_length[i], sizeof(int));
 			verification(verif_read != -1, "[--setheight] read path length");
-
 			for (int j = 0; j <= path_length[i]; ++j) {
+				//on lit son chemin
 				verif_read = read(fd, &path[i][j], sizeof(char));
 				verification(verif_read != -1, "[--setheight] read path");
 			}
-
+			//on lit son nombre de frames
 			verif_read = read(fd, &frames[i], sizeof(unsigned));
 			verification(verif_read != -1, "[--setheight] read nb frames");
 
 			for (int k = 0; k < 4; ++k) {
+				//on lit toutes ses propriétés
 				verif_read = read(fd, &properties[i][k], sizeof(int));
 				verification(verif_read != -1, "[--setheight] read properties");
 			}
 		}
-
+		//on ferme le fichier en ouverture
 		close(fd);
+		//on ouvre le fichier en paramètre en écriture
 		fd = open(argv[1], O_WRONLY|O_TRUNC, 0600);
         verification(fd != -1, "[--setheight] open write/trunc");
 
 		int verif_write;
-
+		//on écrit la largeur 
 		verif_write = write(fd, &width, sizeof(int));
 		verification(verif_write != -1, "[--setheight] write width");
-
+		//on écrit la nouvelle hauteur
 		verif_write = write(fd, &new_height, sizeof(int));
 		verification(verif_write != -1, "[--setheight] write new height");
-
+		//on écrit le nombre d'objet
 		verif_write = write(fd, &nb_objects, sizeof(int));
 		verification(verif_write != -1, "[--setheight] write nb objects");
-
+		//on écrit pour chaque coordonées (x,y) de la carte son type
 		for (int y = 0; y < new_height; ++y) {
 			for (int x = 0; x < width; ++x) {
 				verif_write = write(fd, &new_matrix[x][y], sizeof(int));
 				verification(verif_write != -1, "[--setheight] write new matrix");
 			}
 		}
-
+		//pour chaque objet 
 		for (int i = 0; i < nb_objects; ++i) {
+			//on écrit la longueur de son chemin
 			verif_write = write(fd, &path_length[i], sizeof(int));
 			verification(verif_write != -1, "[--setheight] write path length");
-
+			//on écrit son chemin
 			for (int j = 0; j <= path_length[i]; ++j) {
 				verif_write = write(fd, &path[i][j], sizeof(char));
 				verification(verif_write != -1, "[--setheight] write path");
 			}
-
+			//on écrit son nombre de frames
 			verif_write = write(fd, &frames[i], sizeof(unsigned));
 			verification(verif_write != -1, "[--setheight] write frames");
 
-			for (int k = 0; k < 4; ++k) {
+			for (int k = 0; k < 4; ++k) {	
+				//on écrit toutes ses propriétés
 				verif_write = write(fd, &properties[i][k], sizeof(int));
 				verification(verif_write != -1, "[--setheight] write properties");
 			}
 		}
+		//on ferme le fichier
 		close(fd);
 		return 0;
 	}
+	//utilisation de l'option --setobjects
 	else if (strcmp(argv[2], "--setobjects") == 0){
-
+		//on ouvre le fichier en paramètre 
         int fd = open(argv[1], O_RDONLY);
         verification(fd != -1, "[--setobjects] open read");
 
@@ -334,26 +372,26 @@ int main (int argc, char** argv)
         int width;
         int height;
         int old_nb_objects;
-
+        //on lit la largeur
         verif_read = read(fd, &width, sizeof(int));
         verification(verif_read != 1, "[--setobjects] read width");
-
+        //on lit la hauteur
         verif_read = read(fd, &height, sizeof(int));
         verification(verif_read != 1, "[--setobjects] read height");
-
+        //on lit l'ancien nombre d'objet 
         verif_read = read(fd, &old_nb_objects, sizeof(int));
         verification(verif_read != -1, "[--setobjects] read old nb objects");
-
+        //on ferme le fichier
         close(fd);
 
 		int nb_args = argc - 3;
         int new_nb_objects = nb_args/6;
-        if (nb_args%6 != 0){
+        if (nb_args%6 != 0) {
             fprintf(stderr, "Nombre d'arguments incorrect.\n"
                     "Usage : %s <file> --setobjects { <filename> <frames> <solidity>\n"
                     "<destructible> <collectible> <generator> }", argv[0]);
             return 1;
-        } else if (new_nb_objects < old_nb_objects){
+        } else if (new_nb_objects < old_nb_objects) {
             fprintf(stderr, "Le nombre de nouveaux objets doit être égal ou supérieur au nombre d'objets initial.\n"
                     "Nombre d'objets initial : %d", old_nb_objects);
             return 1;
